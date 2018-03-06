2e0ef999f15e64729c9380bd10c888ba27582944
krzychna <kkrzys17@gmail.com>
2018-03-06 21:45:47 +0100

    Wskazniki XIV

--- main.cpp
+++ main.cpp
@@ -1,9 +1,9 @@
 #include <iostream>
 using namespace std;
 int main() {
-    int *wsk;
-    int wsk_n = 0xa2ff4;
-    wsk = reinterpret_cast<int*> (wsk_n);
-    cout << wsk;
+    char cc;
+    void *wv = &cc;
+    char *wc = &cc;
+
     return 0;
 }
