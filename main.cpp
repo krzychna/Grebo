e7354c2bb4d4b49d8cbdda63fbb5259c1bfe8ca5
krzychna <kkrzys17@gmail.com>
2018-03-06 21:49:15 +0100

    Wskazniki XVI

--- main.cpp
+++ main.cpp
@@ -1,9 +1,17 @@
 #include <iostream>
 using namespace std;
 int main() {
-    char cc;
-    void *wv = &cc;
-    char *wc = &cc;
+    int *wi = nullptr;
+    if (wi!= nullptr)
+    {
+        cout<<"rozne od nullptr";
+    }else
+        cout<<"nullptr";
+    if (wi)
+    {
+        cout<<"rozne od nullptr";
+    }else
+        cout<<"nullptr";
 
     return 0;
 }
