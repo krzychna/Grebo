7ebafaeae6ffe3672520e8b10df95c50702e3258
krzychna <kkrzys17@gmail.com>
2018-03-06 19:38:15 +0100

    Przesylanie przez terminal

---
+++ cmake-build-debug/CMakeFiles/Grebo.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Grebo.dir/main.cpp.o"
+  "Grebo.pdb"
+  "Grebo"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Grebo.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
