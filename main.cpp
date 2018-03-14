04b45bd067210969171f4e8cc68c7d8de8ecb216
krzychna <kkrzys17@gmail.com>
2018-03-14 21:07:11 +0100

    Wskazniki runda 2

--- main.cpp
+++ main.cpp
@@ -1,13 +1,16 @@
 #include <iostream>
 using namespace std;
 
+
+double *kreator_tablicy(int rozmiar);
 void fun (double *tab1,double *tab2, double *tab3, int rozmiar);
 
 int main() {
+    auto *wsk = new int;
     const int rozmiar = 20;
-    double *wsk1 = new double[rozmiar];
-    double *wsk2 = new double[rozmiar];
-    double *wsk3 = new double[rozmiar];
+    double *wsk1 = kreator_tablicy(rozmiar);
+    double *wsk2 = kreator_tablicy(rozmiar);
+    double *wsk3 = kreator_tablicy(rozmiar);
     for (int i = 0; i <rozmiar ; ++i) {
         *(wsk1+i) = i;
         *(wsk2+i) = 2*i;
@@ -26,7 +29,9 @@ int main() {
 
 }
 
-
+double *kreator_tablicy(int rozmiar){
+    return new double[rozmiar];
+}
 void fun (double *tab1,double *tab2, double *tab3, int rozmiar){
     for (int i = 0; i <rozmiar ; ++i) {
         *(tab3+i) = *(tab1+i)*(*(tab2+i));
