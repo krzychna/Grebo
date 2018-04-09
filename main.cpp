cce8b7034128ef83a45837a2a603c95b059fdd06
krzychna <kkrzys17@gmail.com>
2018-04-09 21:47:38 +0200

    str 604 XXIII

--- main.cpp
+++ main.cpp
@@ -1,111 +1,21 @@
 #include <iostream>
+#include <string>
 using namespace std;
-#include <vector>
-
-bool zgiecie_ramienia();
-bool obrot_ramienia();
-
-bool zgiecie_przedramienia();
-bool obrot_przedramienia();
-
-bool zgiecie_nadgarstka();
-bool obrot_nadgarstka();
-
-int wybor =0;
-int main() {
-    vector <bool (*)()> sekwencja_wsk;
-    vector <double> sekwencja_kat;
-    double kat;
-    while(true){
-        cout<<"Podaj kolejnosc funkcji w jakiej maja sie wykonac"
-                "\n"
-                "Do wyboru: \n"
-                "[1] zgiecie_ramienia \n"
-                "[2] obrot_ramienia \n"
-                "[3] zgiecie_przedramienia \n"
-                "[4] obrot_przedramienia \n"
-                "[5] zgiecie_nadgarstka \n"
-                "[6] obrot_nadgarstka \n"
-                "[7] Wykonaj wybrana sekwencje\n"
-                "[8] Wyczysc\n"
-                "[0] Koniec\n";
-        cin>>wybor;
-        switch  (wybor){
-            case 0:
-                return 0;
-            case 1:
-                sekwencja_wsk.push_back(&zgiecie_ramienia);
-                cin>>kat;
-                sekwencja_kat.push_back(kat);
-                break;
-            case 2:
-                sekwencja_wsk.push_back(&obrot_ramienia);
-                cin>>kat;
-                sekwencja_kat.push_back(kat);
-                break;
-            case 3:
-                sekwencja_wsk.push_back(&zgiecie_przedramienia);
-                cin>>kat;
-                sekwencja_kat.push_back(kat);
-                break;
-            case 4:
-                sekwencja_wsk.push_back(&obrot_przedramienia);
-                cin>>kat;
-                sekwencja_kat.push_back(kat);
-                break;
-            case 5:
-                sekwencja_wsk.push_back(&zgiecie_nadgarstka);
-                cin>>kat;
-                sekwencja_kat.push_back(kat);
-                break;
-            case 6:
-                sekwencja_wsk.push_back(&obrot_nadgarstka);
-                cin>>kat;
-                sekwencja_kat.push_back(kat);
-                break;
-            case 7:
-                for (int i = 0; i < sekwencja_wsk.size() ; i++) {
-                    (sekwencja_wsk[i])();
-                    cout<<"o "<< sekwencja_kat[i] <<" stopni"<<endl;
-                }
-                break;
-            case 8:
-                sekwencja_wsk.clear();
-                sekwencja_kat.clear();
-                break;
-        }
-    }
-
-    return 0;
-
+int main(){
+    string stacja1;
+    string stacja2;
+    int klasa =2;
+    cout<<"Skad jedziesz?\n";
+    cin>>stacja1;
+    cout<<"Dokad jedziesz?\n";
+    cin>>stacja2;
+    cout<<"Jaka klasa?\n";
+    cin>>klasa;
+    string kreska = "*" +string(48,'-') + "*";
+    string komunikat = "|" + stacja1 + "--->" +stacja2+ ", klasa" + to_string(klasa);
+    string spacje = string(49-komunikat.size(),' ') + "|";
+    cout<< kreska<<endl;
+    cout<< komunikat<<spacje<<endl;
+    cout<<kreska;
 }
-
-
-bool zgiecie_ramienia(){
-    cout<<"zgiecie_ramienia ";
-    return true;
-}
-bool obrot_ramienia(){
-    cout<<"obrot_ramienia ";
-    return true;
-}
-
-bool zgiecie_przedramienia(){
-    cout<<"zgiecie_przedramienia ";
-    return true;
-}
-bool obrot_przedramienia(){
-    cout<<"obrot_przedramienia ";
-    return true;
-}
-
-bool zgiecie_nadgarstka(){
-    cout<<"zgiecie_nadgarstka ";
-    return true;
-}
-bool obrot_nadgarstka(){
-    cout<<"obrot_nadgarstka ";
-    return true;
-}
-
 
