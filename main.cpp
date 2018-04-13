679107e68ba383cd2f32ae91db193ea60c9effa4
krzychna <kkrzys17@gmail.com>
2018-04-13 16:03:39 +0200

    str 840 XXIV

--- main.cpp
+++ main.cpp
@@ -1,18 +1,9 @@
 #include <iostream>
+#include "klasy.h"
 using namespace std;
 
 int main (){
-    int** wsk = new int*[8];
-    for (int i = 0; i <8 ; ++i) {
-        *(wsk+i)= new int;
-        *(*(wsk+i)) = i;
-        cout<<*(*(wsk+i));
-    }
-    for (int i = 0; i <8 ; ++i) {
-        delete *(wsk+i);
-
-    }
-    delete [] wsk;
+    Tsamochod autobus;
     return 0;
 }
 
